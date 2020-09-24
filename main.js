cb8f80094cfbdf740f55dfc90ef29cdc6396d296
Nathaniel Bedassie <nathanielbedassie@gmail.com>
2020-09-23 20:44:07 -0500

    started working on transistions

---
+++ main.js
@@ -0,0 +1,10 @@
+var yes_button = document.getElementById("initiator-yes-button");
+var no_button = document.getElementById("initiator-no-button");
+var navigation = document.getElementById("navigator");
+var initiator = document.getElementById("initiator");
+
+yes_button.onclick = function(){
+    initiator.style.display = "none";
+    navigation.style.display = "block";
+    console.log("Did it work?");
+}
